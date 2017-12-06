32eda55c178605ef7ccb20b29c20dff2d3e35b62
Seul Lee <skdidy9191@naver.com>
2017-12-07 01:53:37 +0900

    password...

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app=Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'toor'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'DesignProjectD_Pet'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_CHARSET'] = 'utf-8'
